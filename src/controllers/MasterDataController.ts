b873630227f7da98131790c4ca81bd50e545e07f
nguyenthanh.cong7 <nguyenthanh.cong7@vietcredit.com.vn>
2024-06-13 17:39:11 +0700

    ..

--- src/controllers/MasterDataController.ts
+++ src/controllers/MasterDataController.ts
@@ -4,8 +4,51 @@ class MasterDataController {
   async GetByKey(req, res) {
     try {
       const data = {
+
         logoUrl: "dgb-logo.png",
-        shopName: "Dragonball shop",
+        shopName: "",
+        menuItems: [
+          {
+            title: "Kho tài khoản",
+            subTitle: "games",
+            subMenuItems: [
+              {
+                title: "Acc sẵn char giá rẻ đến vip",
+                href: "#BN-5NWKQO",
+              },
+              {
+                title: "Acc reroll abc",
+                href: "#BN-B8AXNI",
+              },
+              {
+                title: "Acc trên 50k",
+                href: "#BN-W8VJQQ",
+              },
+              {
+                title: "Acc trên 100k",
+                href: "#BN-MPIWVW",
+              },
+            ],
+          },
+          {
+            title: "Tính năng",
+            subTitle: "info",
+            subMenuItems: [
+              {
+                title: "Hướng dẫn thanh toán",
+                href: "/",
+              },
+              {
+                title: "Hướng dẫn mua acc",
+                href: "/",
+              },
+            ],
+          },
+        ],
+        highLightItem: {
+          title: "Nạp thẻ",
+          href: "/account-management/recharge",
+        },
         sliders: [
           {
             code: utils.generateUniqueString(6),
